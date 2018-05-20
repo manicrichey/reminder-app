1cd30472b9a7a830fe8b83873e23be6a530dcb95
Richard Green <manicrichey@users.noreply.github.com>
2018-05-20 21:47:25 +0100

    Code clean up and readme file added

--- app.js
+++ app.js
@@ -3,42 +3,27 @@ var test = document.querySelectorAll('input');
 var buttonhook = document.querySelector('#enterdata');
 var placeholder = document.querySelector('#placeholder');
 
-var todolist = [{reminder:"billy", date:55}];
+var todolist = [];
 
+// This function didn't work for some reason. I need to put it through the dev tools
 // function arrayToString(array){
 //     array.join(" ");
 // }
 
-//console.log(arrayToString(todolist));
-
 function addlistelement(){
+    //Use the .length value to just add the last element to the page
     //console.log(todolist.length);
-    //console.log(todolist);
+    
     var myarray = todolist.map(function(cur,i,arr){
         return "<li>" + cur.reminder + "</li>";
-        //console.log(cur.reminder);
-        
-
     });
 
     return myarray.join(" ");
-    //console.log(myarray.join(" "));
-    //console.log(arrayToString(myarray));
+    
 }
 function getReminderValues(){
-    //console.log("button has been clicked - rg");
-    placeholder.innerHTML = "button has been clicked";
-    //console.log(test[0].value);
     todolist.push({reminder:test[0].value, date:test[1].value});
-    
-    console.log(addlistelement());
     placeholder.innerHTML = addlistelement();
-    //console.log(test);
-
-
 } 
+
 buttonhook.addEventListener('click', getReminderValues,false);
-
-
-//console.log(buttonhook);
-//console.log(test);
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Reminder App | City University Course
+
+A simple app to record 'reminders'. The form will collect two fields. Reminder text and the date.
+
+## Things to do next
+
+1. Add css to the project
+1. Use the .length value of the array to just add the value entered.
+1. Get rid of the form default value when you enter the textbox.
+1. Add a delete function.
+1. Add an edit function. 
+1. Use some sort of form validation to check if the items are empty.
