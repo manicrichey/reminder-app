91813202b3c30e1b36a8c079dffeed8acf303924
Richard Green <manicrichey@users.noreply.github.com>
2018-05-21 20:47:18 +0100

    Added event listener on delete text

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var test = document.querySelectorAll('input');
+var errormsg = document.querySelector('#errormsg');
+
 
 
 var buttonhook = document.querySelector('#enterdata');
-var placeholder = document.querySelector('#placeholder');
+var placeholder = document.querySelector('#placeholder tbody');
+console.log(placeholder);
 
 var todolist = [];
 
 //Object Constructor
 function reminderObject(reminderText, reminderDate){
+    this.id = todolist.length+1;
     this.reminder = reminderText;
     this.date = reminderDate;
     this.timestamp = Date.now();
@@ -16,21 +20,43 @@ function reminderObject(reminderText, reminderDate){
 //reminder:test[0].value, date:test[1].value
 function getReminderValues(){
 
-    todolist.push(new reminderObject(test[0].value, test[1].value));
+    if((test[0].value === "Enter a reminder" || test[0].value === "Add another item" || test[0].value === ' ' || test[1].value === '')){
+        errormsg.textContent = "You have to fill out both fields"
+    } else {
+        errormsg.textContent = " ";
+        todolist.push(new reminderObject(test[0].value, test[1].value));
 
     // CAll function to add list items to the DOM
-    placeholder.innerHTML = addlistelement();
+    
+    placeholder.insertAdjacentHTML('beforeend', addlistelement());
+    setlistener();
     test[0].value = "Add another item";
+    test[1].value = "";
     console.log(todolist);
+    var deletekey = document.querySelectorAll('tr td[data-test]');
+    for(i=0; i < deletekey.length; i++){ 
+        //console.log(deletekey[i].getAttribute('data-test'));
+    };
+    //console.log(document.querySelectorAll('tr td[data-test]'));
+    }
+
+    
 } 
 
 //Add items to the DOM
 function addlistelement(){
     
     var myarray = todolist.map(function(cur,i,arr){
-        return "<li>" + cur.reminder + "</li>";
+        if(i == todolist.length-1){
+            return "<tr>" + "<td>" + cur.id + "</td>" + "<td>" + cur.reminder + "</td>" + "<td>" + cur.date + "</td>" + "<td data-test='" + (cur.id-1) + "'>" + "Delete" + "</td>" +"</tr>";
+        }
+        
     });
 
+    // myarray.unshift("<tr>");
+    // myarray.push("</tr>");
+    console.log(myarray);
+    
     return myarray.join(" ");
     
     
@@ -39,9 +65,33 @@ function addlistelement(){
 
 //Event listener for 'submit' button
 buttonhook.addEventListener('click', getReminderValues,false);
+
+//Event listener for 'imput focus'
 test[0].addEventListener('focus', function(){
     console.log("in focus");
     test[0].value = " ";
 })
 
+//var testclick = document.querySelectorAll('tr td[data-test]');
+
+
+function setlistener(){
+    var testclick = document.querySelectorAll('tr td[data-test]');
+    testclick.forEach(function(item, index){
+        item.addEventListener('click', function(){
+            console.log(item.getAttribute('data-test'));
+        })
+    });
+    console.log(document.querySelectorAll('tr td[data-test]'));
+    // testclick.addEventListener('click', function(){
+    //     console.log("clicked");
+    // });
+}
+
+
+
+//Event listener for delete
+
+
+//sconsole.log("date = " + test[1].value);
 
