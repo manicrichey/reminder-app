a597970acbb2f731293af852c3243dd4d9a26949
Richard Green <manicrichey@users.noreply.github.com>
2018-05-23 00:00:10 +0100

    Version 1 Complete

--- app.js
+++ app.js
@@ -6,23 +6,22 @@ const inputTextBoxes = document.querySelectorAll('input');
 const errormsg = document.querySelector('#errormsg');
 //This is the button used to 'submit' a new reminder
 const buttonhook = document.querySelector('#enterdata');
-//This is the place in the DOM to append a new reminder
-const placeholder = document.querySelector('#placeholder tbody');
-
+//This is the place in the DOM to append a new reminder as a <tr>
+const tablehook = document.querySelector('#tablehook tbody');
+console.log(tablehook.firstChild);
 //initialise todolist array
 var todolist = [];
-//initialise setter array
-var setter = [];
+//initialise deleteBottonNodeList array
+var deleteBottonNodeList = [];
 
 //Object Constructor (template) for a reminder item
 function reminderObject(reminderText, reminderDate){
-    this.id = todolist.length+1;
+    this.id = todolist.length;
     this.reminder = reminderText;
     this.date = reminderDate;
     this.timestamp = Date.now();
 };
 
-//reminder:test[0].value, date:test[1].value
 function getReminderValues(){
 
     if((inputTextBoxes[0].value === "Enter a reminder" || inputTextBoxes[0].value === "Add another item" || inputTextBoxes[0].value === ' ' || inputTextBoxes[1].value === '')){
@@ -31,80 +30,90 @@ function getReminderValues(){
         errormsg.textContent = " ";
         todolist.push(new reminderObject(inputTextBoxes[0].value, inputTextBoxes[1].value));
 
+    } 
+}
+
+function getDeleteButtons(){
+    return document.querySelectorAll('tr td[data-test]');
+}
+
+//Delete a reminder item from the array and remove the item from the DOM
+function deleteReminder(){
+    //This removes the item from the dom and the item from the todolist   
+    console.log("deleting... ", parseInt(this.getAttribute('data-test')));
+    todolist.splice(parseInt(this.getAttribute('data-test')),1);
+    addNewReminder('nocheck');
+}
+
+function insertElement(){
     // CAll function to add list items to the DOM
-    placeholder.insertAdjacentHTML('beforeend', addlistelement());
-    setter = getels()
-    removals();
-    addition();
+    tablehook.insertAdjacentHTML('beforeend', addlistelement());
+    //Reset the event listener
+    deleteBottonNodeList = getDeleteButtons()
+    removeDeleteButtonListener();
+    addDeleteButtonListener();
     inputTextBoxes[0].value = "Add another item";
     inputTextBoxes[1].value = "";
     var deletekey = document.querySelectorAll('tr td[data-test]');
-
-    }
-    
-} 
-
-//Add items to the DOM using array.map to add html tags
-function addlistelement(){
-    
-    var myarray = todolist.map(function(cur,i,arr){
-        if(i == todolist.length-1){
-            return "<tr class='dynamic'>" + "<td>" + cur.id + "</td>" + "<td>" + cur.reminder + "</td>" + "<td>" + cur.date + "</td>" + "<td data-test='" + (cur.id-1) + "'>" + "Delete" + "</td>" +"</tr>";
-        }     
-    });
-
-    return myarray.join(" ");
-    
 }
 
-//Recreates a new list after an item is deleted
-function redrawTable(){
-    var myredrawnArray = todolist.map(function(cur,i,arr){
+function addlistelement(){
+    //Clear previous elements and render list 
+    while (tablehook.childNodes.length > 1) {
+        tablehook.removeChild(tablehook.lastChild);
+    }
+    //Map the todolist array and add html table tags
+    var ArrayToHTML = todolist.map(function(cur,i,arr){
         
-            return "<tr class='dynamic'>" + "<td>" + cur.id + "</td>" + "<td>" + cur.reminder + "</td>" + "<td>" + cur.date + "</td>" + "<td data-test='" + (cur.id-1) + "'>" + "Delete" + "</td>" +"</tr>";
-
+        return "<tr class='dynamic'>" + "<td>" + (i+1) + "</td>" + "<td>" + cur.reminder + "</td>" + "<td>" + cur.date + "</td>" + "<td data-test='" + (i) + "'>" + "<button>Delete</button>" + "</td>" +"</tr>";
+      
     });
-    return myredrawnArray.join(" ");
+
+    return ArrayToHTML.join(" ");
+       
+}
+
+function addNewReminder(nocheck){
+    //Use this to enable form validation
+    if(nocheck === true){
+        getReminderValues();
+    }
+    insertElement();
+    console.table(todolist);
 }
 
 //EVENT LISTENERS
 
 //Event listener for 'submit' button
-buttonhook.addEventListener('click', getReminderValues,false);
+buttonhook.addEventListener('click', function(){
+    addNewReminder(true);
+},false);
 
 //Event listener for 'imput focus'
 inputTextBoxes[0].addEventListener('focus', function(){
    inputTextBoxes[0].value = " ";
 })
 
-
-function getels(){
-    return document.querySelectorAll('tr td[data-test]');
-}
-
-//Delete a reminder item from the array and remove the item from the DOM
-function deleteReminder(){
-    //This removes the item from the dom and the item from the todolist
-
-    //console.log("about to parse ", parseInt(this.getAttribute('data-test')));
-    todolist.splice(parseInt(this.getAttribute('data-test')),1);
-    var top = this.parentNode.parentNode;
-    var nest = this.parentNode;
-    top.removeChild(nest);
-    console.table(todolist);
-}
+//Event listener for leaving 'imput focus'
+// inputTextBoxes[0].addEventListener('blur', function(){
+//     inputTextBoxes[0].value = "Enter a reminder";
+//  })
 
 //remove/unset event listener for all 'Delete' bottons
-function removals(){
-    setter.forEach(function(item, index){
+function removeDeleteButtonListener(){
+    deleteBottonNodeList.forEach(function(item, index){
         item.removeEventListener('click', deleteReminder);
     });
 }
 
 //add event listeners for all 'Delete' buttons
-function addition(){
-    setter.forEach(function(item, index){
+function addDeleteButtonListener(){
+    deleteBottonNodeList.forEach(function(item, index){
         item.addEventListener('click', deleteReminder);
     });
 }
+
+
+
+
 
