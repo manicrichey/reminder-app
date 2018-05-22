2005ed18be8ea6aa7bcd973202a1eb30f9c36ae9
Richard Green <rich.green@gmx.com>
2018-05-22 13:51:52 +0100

    Styling Changes and some code refactoring. It still doesnt work as intended

--- app.js
+++ app.js
@@ -1,47 +1,46 @@
-var test = document.querySelectorAll('input');
-var errormsg = document.querySelector('#errormsg');
+//GET DOM ELEMENTS TO WORK WITH
 
+//Get all the form input boxes
+const inputTextBoxes = document.querySelectorAll('input');
+//Use this to display an message when form fields are left empty
+const errormsg = document.querySelector('#errormsg');
+//This is the button used to 'submit' a new reminder
+const buttonhook = document.querySelector('#enterdata');
+//This is the place in the DOM to append a new reminder
+const placeholder = document.querySelector('#placeholder tbody');
+
+//initialise todolist array
+var todolist = [];
+//initialise setter array
 var setter = [];
 
-var buttonhook = document.querySelector('#enterdata');
-var placeholder = document.querySelector('#placeholder tbody');
-//console.log(placeholder);
-
-var todolist = [];
-
-//Object Constructor
+//Object Constructor (template) for a reminder item
 function reminderObject(reminderText, reminderDate){
     this.id = todolist.length+1;
     this.reminder = reminderText;
     this.date = reminderDate;
     this.timestamp = Date.now();
-    };
+};
 
 //reminder:test[0].value, date:test[1].value
 function getReminderValues(){
 
-    if((test[0].value === "Enter a reminder" || test[0].value === "Add another item" || test[0].value === ' ' || test[1].value === '')){
+    if((inputTextBoxes[0].value === "Enter a reminder" || inputTextBoxes[0].value === "Add another item" || inputTextBoxes[0].value === ' ' || inputTextBoxes[1].value === '')){
         errormsg.textContent = "You have to fill out both fields"
     } else {
         errormsg.textContent = " ";
-        todolist.push(new reminderObject(test[0].value, test[1].value));
+        todolist.push(new reminderObject(inputTextBoxes[0].value, inputTextBoxes[1].value));
 
     // CAll function to add list items to the DOM
-    
     placeholder.insertAdjacentHTML('beforeend', addlistelement());
     setter = getels()
     removals();
     addition();
-    test[0].value = "Add another item";
-    test[1].value = "";
-    //console.log(todolist);
+    inputTextBoxes[0].value = "Add another item";
+    inputTextBoxes[1].value = "";
     var deletekey = document.querySelectorAll('tr td[data-test]');
-    for(i=0; i < deletekey.length; i++){ 
-        //console.log(deletekey[i].getAttribute('data-test'));
-    };
-    //console.log(document.querySelectorAll('tr td[data-test]'));
-    }
 
+    }
     
 } 
 
@@ -51,78 +50,61 @@ function addlistelement(){
     var myarray = todolist.map(function(cur,i,arr){
         if(i == todolist.length-1){
             return "<tr class='dynamic'>" + "<td>" + cur.id + "</td>" + "<td>" + cur.reminder + "</td>" + "<td>" + cur.date + "</td>" + "<td data-test='" + (cur.id-1) + "'>" + "Delete" + "</td>" +"</tr>";
-        }
-        
+        }     
     });
 
-    // myarray.unshift("<tr>");
-    // myarray.push("</tr>");
-    //console.log(myarray);
-    
     return myarray.join(" ");
-    
     
 }
 
+//Recreates a new list after an item is deleted
 function redrawTable(){
     var myredrawnArray = todolist.map(function(cur,i,arr){
         
             return "<tr class='dynamic'>" + "<td>" + cur.id + "</td>" + "<td>" + cur.reminder + "</td>" + "<td>" + cur.date + "</td>" + "<td data-test='" + (cur.id-1) + "'>" + "Delete" + "</td>" +"</tr>";
-        
-        
+
     });
     return myredrawnArray.join(" ");
 }
 
+//EVENT LISTENERS
 
 //Event listener for 'submit' button
 buttonhook.addEventListener('click', getReminderValues,false);
 
 //Event listener for 'imput focus'
-test[0].addEventListener('focus', function(){
-   // console.log("in focus");
-    test[0].value = " ";
+inputTextBoxes[0].addEventListener('focus', function(){
+   inputTextBoxes[0].value = " ";
 })
 
-//var testclick = document.querySelectorAll('tr td[data-test]');
-
-//var testclick = document.querySelectorAll('tr td[data-test]');
 
 function getels(){
     return document.querySelectorAll('tr td[data-test]');
 }
 
-function logger(){
+//Delete a reminder item from the array and remove the item from the DOM
+function deleteReminder(){
     //This removes the item from the dom and the item from the todolist
-    console.log("about to parse ", parseInt(this.getAttribute('data-test')));
+
+    //console.log("about to parse ", parseInt(this.getAttribute('data-test')));
     todolist.splice(parseInt(this.getAttribute('data-test')),1);
     var top = this.parentNode.parentNode;
     var nest = this.parentNode;
     top.removeChild(nest);
     console.table(todolist);
-
-    
-
 }
 
+//remove/unset event listener for all 'Delete' bottons
 function removals(){
     setter.forEach(function(item, index){
-        item.removeEventListener('click', logger)
+        item.removeEventListener('click', deleteReminder);
     });
 }
 
+//add event listeners for all 'Delete' buttons
 function addition(){
     setter.forEach(function(item, index){
-        item.addEventListener('click', logger)
+        item.addEventListener('click', deleteReminder);
     });
 }
-
-
-
-
-
-//Event listener for delete
-
-
-//sconsole.log("date = " + test[1].value);
 
--- readme.md
+++ readme.md
@@ -12,3 +12,11 @@ A simple app to record 'reminders'. The form will collect two fields. Reminder t
 1. Use some sort of form validation to check if the items are empty.
 1. Create a valid form and prevent default
 1. Clear last entry from text field when submit is pressed
+
+## 2018-05-22
+
+### Things to do this evening
+
+- Refactor the 'getReminderValues' function. Split it apart to get the values then pass them onto another function to add them to the todolist array
+- I need a hook to get to the item I need when deleting an item. The data-test attribute I am using doesnt work in its current form and the ID's are overlapping Maybe I need to redraw the list each time I add a new item?
+- Style the table a little just so it actually looks like a proper table.
